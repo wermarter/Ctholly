3ac89618ebbb6184bafd56e73b6a0ab282ca61a1
Ha Minh Chien <wermarter@gmail.com>
2018-11-15 22:25:37 +0700

    merge htm and hvn

--- main.py
+++ main.py
@@ -8,7 +8,27 @@ logging.captureWarnings(True)
 
 from downloader import BatchDownloader
 
+def post_processing(files):
+    print('Cropping to 720p')
+    for file in files:
+        utils.crop_to_720p(file)
 
+def fetch_htm(url):
+    url = url.replace('galleries', 'reader')
+    book_id = int(re.findall(r"reader/(.+).html", url)[0][-1])
+    if book_id == 1:
+        book_id = 0
+    IMG_PREFIX = "https://"+ ("a" if (book_id % 2 == 0) else "b") + "a"
+    html = utils.fetch_html(url)
+    title = re.findall(r"<title>(.+) \| Hitomi.la</title>", html)[0]
+    res = re.findall(r"<div class=\"img-url\">//g(.+?)</div>", html)
+    img_urls = [IMG_PREFIX + s for s in res]
+    print('Fetching', title, len(img_urls))
+    bd = BatchDownloader(img_urls, title, 'numeric')
+    bd.run()
+    post_processing(bd.file_dests)
+
+_HVN = "https://hentaivn.net"
 def fetch_hvn(url, title=None):
     html = utils.fetch_html(url)
     if not title:
@@ -19,9 +39,7 @@ def fetch_hvn(url, title=None):
         print('Fetching', title, len(img_urls))
         bd = BatchDownloader(img_urls, title, 'numeric')
         bd.run()
-        print("Postprocessing")
-        for file_dest in bd.file_dests:
-            utils.crop_to_720p(file_dest)
+        post_processing(bd.file_dests)
     else:
         title = re.findall(r"<title>(.+) \| Đọc Online</title>", html)[0][15:]
         chap_urls = re.findall(r"href=\"(.+?)\"><h2 class=\"chuong_t\"", html)
@@ -29,13 +47,18 @@ def fetch_hvn(url, title=None):
         assert len(chap_titles) == len(chap_urls)
         print('Fetching', title, len(chap_urls))
         for url, chap in zip(chap_urls, chap_titles):
-            fetch_hvn("https://hentaivn.net"+url, chap)
+            fetch_hvn(_HVN+url, chap)
             for file in os.listdir(chap):
                 _dest = os.path.join(title, chap)
                 os.makedirs(_dest, exist_ok=True)
                 shutil.copy(os.path.join(chap, file), _dest)
             shutil.rmtree(chap)
 
+def fetch(url):
+    if url.startswith(_HVN):
+        fetch_hvn(url)
+    else:
+        fetch_htm(url)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -44,12 +67,14 @@ if __name__ == '__main__':
     else:
         cmd = str(input('>'))
     if utils.is_html(cmd):
-        fetch_hvn(cmd)
+        fetch(cmd)
+    elif cmd.endswith('.zip'):
+        utils.recompile_htm(cmd, backup=False)
     else:
         with open(cmd, 'r') as f:
             urls = [line.strip() for line in f.readlines()]
             n = len(urls)
             for i, url in enumerate(urls):
                 print('[%d/%d] %s'%(i+1, n, url))
-                fetch_hvn(url)
+                fetch(url)
     print("DONE")
