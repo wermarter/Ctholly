200e9bcadf58badd99030ae1687476db49a1e56f
Ha Minh Chien <wermarter@gmail.com>
2019-07-22 20:17:29 +0700

    add back linux support

---
+++ ctholly.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Add `alias ctholly=${PATH_TO_RUN.SH}` to `.bashrc`
+# will allow you to use Ctholly via `ctholly` in terminal
+
+# Assign install location to DIR
+DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Set terminal title
+echo -en "\033]0;Ctholly\a"
+
+# Run main.py in conda env Ctholly
+clear
+source activate Ctholly
+python ${DIR}/cli.py
